5bd32efb35b70a83a3ef54b768967ac34f991955
ErgunBe <ergun.besli@hotmail.com>
2024-05-19 01:35:19 +0300

    CarpimTablosu

---
+++ carpım_tablosu/carpım_tablosu/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carpım_tablosu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("çarpım tablosunu oluşturmak için bir sayı girşn: ");
+            int sayi=Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++) 
+            {
+                Console.WriteLine($"{sayi} * {i} = {sayi * i}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
